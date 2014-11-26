407fce6f01dff8625485007e5ac3d9a90df78eb6
Miguel Palhas <mpalhas@gmail.com>
2014-11-26 12:46:32 +0000

    Adds templates directory

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.get('/', function(req, res) {
         res.send("Error " + err);
       } else {
         var status = result.rows[0].status;
-        res.render('public/index', {
+        res.render('templates/index', {
           title:     status ? "Someone's in there" : "Go ahead!",
           css_class: status ? "busy" : "free"
         })
