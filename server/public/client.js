6b1f3e88ffa36629a23969f7c809b76378be27c2
Miguel Palhas <mpalhas@gmail.com>
2014-11-28 18:39:02 +0000

    Adds state to page title

--- server/public/client.js
+++ server/public/client.js
@@ -2,4 +2,5 @@ var source = new EventSource("/updates");
 
 source.addEventListener('message', function(e) {
   document.getElementsByTagName('body')[0].className = e.data;
+  document.getElementsByTagName('title')[0].innerHTML = e.data + " - GB WC Occupancy Calculator™";
 }, false);
